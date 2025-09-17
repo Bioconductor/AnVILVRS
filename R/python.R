3552ff94532b95f0a5f8ca7d53cde5e08bae0fe3
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-09-17 15:55:09 -0400

    add .vrs_translator from python script

--- R/python.R
+++ R/python.R
@@ -7,3 +7,16 @@
         .anvilvrs
     }
 })
+
+#' @export
+.vrs_translator <- local({
+    .vrs_translator <- NULL
+    function() {
+        if (is.null(.vrs_translator))
+            .vrs_translator <<- reticulate::import_from_path(
+                module = "vrs_translator",
+                path = system.file("python", package = "AnVILVRS")
+            )
+        .vrs_translator
+    }
+})
