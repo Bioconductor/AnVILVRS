2bf0c4e5d14f1e5eb510fd4026a600735a579091
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-10-10 13:30:48 -0400

    use BiocFileCache to store pop descriptor tsv file

--- R/utils.R
+++ R/utils.R
@@ -1,30 +1,40 @@
-#' Downloads the population descriptor file to the specified directory
+.POP_DESC_URI <- paste0(
+    "gs://fc-2ee2ca2a-a140-48a1-b793-e27badb7945d/",
+    "data_tables/",
+    "population_descriptor.tsv"
+)
+
+#' Downloads the population descriptor file to the BiocFileCache
 #' from a known Google Storage URI.
 #'
-#' @param destdir `character(1)` The local directory where the file should be
-#'  downloaded. Defaults to the current working directory.
+#' @param uri `character(1)` The URI pointing to a Google Storage location where
+#'   the `population_descriptor.tsv` file is hosted.
 #'
 #' @param ... Additional arguments passed to `AnVILGCP::avcopy()`.
 #'
 #' @importFrom AnVILGCP avcopy
+#' @importFrom BiocFileCache BiocFileCache bfcadd bfcquery bfcrpath
+#' @importFrom tools R_user_dir
 #'
 #' @examplesIf interactive()
-#' get_pop_descriptor(destdir = tempdir())
+#' get_pop_descriptor()
 #' @export
-get_pop_descriptor <- function(destdir = ".", ...) {
-    destdir <- normalizePath(destdir)
-    stopifnot(dir.exists(destdir))
-
-    uri <- paste0(
-        "gs://fc-2ee2ca2a-a140-48a1-b793-e27badb7945d/",
-        "data_tables/",
-        "population_descriptor.tsv"
-    )
-    destfile <- file.path(destdir, basename(uri))
-    AnVILGCP::avcopy(
-        source = uri,
-        destination = destfile
-    )
-    message("Downloaded to: ", destfile)
-    destfile
+get_pop_descriptor <- function(uri = .POP_DESC_URI, ...) {
+    cache <- tools::R_user_dir("AnVILVRS", which = "cache")
+    bfc <- BiocFileCache::BiocFileCache(cache)
+    rid <- BiocFileCache::bfcquery(bfc, query = uri, field = "rname")$rid
+    if (!length(rid)) {
+        message("Downloading population descriptor...")
+        destfile <- tempfile(fileext = ".tsv")
+        AnVILGCP::avcopy(
+            source = uri,
+            destination = destfile,
+            ...
+        )
+        rid <- BiocFileCache::bfcadd(bfc, rname = uri, fpath = destfile) |>
+            names()
+    } else {
+        message("Found in BiocFileCache: ", rid)
+    }
+    BiocFileCache::bfcrpath(bfc, rids = rid)
 }
