4a7013869fe156eb3c2f2c7da05dcad01593112d
LiNk-NY <marcel.ramos@sph.cuny.edu>
2025-12-16 17:49:14 -0500

    use https protocol for cloning

--- inst/scripts/clone.sh
+++ inst/scripts/clone.sh
@@ -1,4 +1,4 @@
 ## cd ~/bioc/anvilvrsproj
 
-git clone git@github.com:gks-anvil/vrs_anvil_toolkit.git
-git clone git@github.com:ga4gh/vrs-python.git
+git clone https://github.com/gks-anvil/vrs_anvil_toolkit.git
+git clone https://github.com/ga4gh/vrs-python.git
